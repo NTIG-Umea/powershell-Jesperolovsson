9872d90a5174c3af3e39238204b5a37f22943684
jesperolovsson <jesper.olovsson@elev.ga.ntig.se>
2023-09-14 15:01:29 +0200

    loopar

---
+++ For.ps1
@@ -0,0 +1,5 @@
+#FOR-loop repeterar ett bestämt antal gånger 
+
+for ($i = 0; $i -lt 20; $i++) {
+    Write-Output $i;
+}
---
+++ while.ps1
@@ -0,0 +1,8 @@
+#While-loop: repeterar så länge någonting stämmer, slutar när det ändras 
+
+$counter = 0;
+
+while ($counter -lt 20) {
+    $counter = $counter + 5;
+    write-output $counter;
+}
