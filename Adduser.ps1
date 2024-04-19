2763020ff0e441de0a22c80230f2cda7a8ab79f9
jesper <jesper.olovsson@elev.ga.ntig.se>
2024-04-19 13:10:47 +0200

    Crazu

--- Adduser.ps1
+++ Adduser.ps1
@@ -1,3 +1,24 @@
-﻿New-ADUser -Name "Olofsoon" -AccountPassword (convertTo-SecureString "Olofsson06" -AsPlainText -Force) -Enabled $true -Path "OU=jespers användare,DC=Walters, DC=Labb"
+﻿$users = Import-Csv .\Newusers.csv  
+foreach ($user in $users) {
+    $user = $user.firstname + $user.lastname
+    $password = $user.password
+    #New-ADUser -Name $user -Path "OU=jespers användare,DC=Walters, DC=Labb"
+    Write-output $user
+
+ 
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+#New-ADUser -Name "Olofsoon" -AccountPassword (convertTo-SecureString "Olofsson06" -AsPlainText -Force) -Enabled $true -Path "OU=jespers användare,DC=Walters, DC=Labb"
 
 
--- userlist.ps1
+++ userlist.ps1
@@ -1,4 +1,4 @@
-$users = Import-Csv userlist.csv | select *, username,mail,password
+$use = Import-Csv userlist.csv | select *, username,mail,password
 $characters = "a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","A","B","C"
 
 
