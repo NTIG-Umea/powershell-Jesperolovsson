4d5194f8720a63fe6092601d6a361ffc8a5c2f4f
jesper <jesper.olovsson@elev.ga.ntig.se>
2024-05-03 12:56:16 +0200

    thisisfinalend

--- assignfolder.ps1
+++ assignfolder.ps1
@@ -1,1 +1,15 @@
-Add-ADGroupMember -Identity "Jespers kompisar" -Members "Petergriffin"
+#Add-ADGroupMember -Identity "Jespers kompisar" -Members "Petergriffin"
+
+#New-Item -Path "\\adc-a\shares\Jespermap2" -Itemtype Directory 
+
+set-Group -identity "jespers kompisar" 
+$permission = Get-Acl "\\adc-a\shares\Jespermap2"
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Jespers kompisar", "Read","Allow")
+$permission.SerAccessRule($rule)
+Set-Acl "\\adc-a\shares\Jespermap2" $permission
+
+
+
+
+
+
